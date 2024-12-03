af9fbdfc887d52165062573a27770f883002328e
vaibhav agarwal <76172874+VaibhavAgarwal877@users.noreply.github.com>
2024-12-04 04:02:17 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull docker pull vaibhav877/simple-python-app-vaib:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 docker pull vaibhav877/simple-python-app-vaib:latest
+docker run -d -p 5000:5000 vaibhav877/simple-python-app-vaib:latest
